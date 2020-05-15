9b5c6228614ed8b687fec1de6d85ebda811dd8d2
root <hirenkavad@gmail.com>
2020-05-15 19:36:27 +0530

    products and orders

--- database/migrations/2020_05_12_151715_create_order_products_table.php
+++ database/migrations/2020_05_12_151715_create_order_products_table.php
@@ -16,7 +16,7 @@ class CreateOrderProductsTable extends Migration
         Schema::create('order_products', function (Blueprint $table) {
             $table->id();
             $table->integer('order_id');
-            $table->integer('product_id';
+            $table->integer('product_id');
             $table->integer('qty');
             $table->decimal('amount', 8, 2)->default(0);
             $table->decimal('gst', 8, 2)->default(0);
