7d3eb59349c7fc0787f6034ef22b0fc37e36d6d7
jignesh <jignesh@m.infex.in>
2020-07-20 19:09:51 +0530

    url change in setings

--- resources/js/settings.js
+++ resources/js/settings.js
@@ -7,7 +7,7 @@ export default {
   title: 'Wyntash',
   theme: variables.theme,
   totalLegs:4,
-  baseUrl:'https://business.wyntash.in/',
+  baseUrl:'http://wyntash-soft.mlmsoftwaredemo.org/',
   /**
    * @type {boolean} true | false
    * @description Whether show the settings right-panel
