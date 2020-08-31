ce4b11b763ee80ce472d95ffa927b77b94a354a4
root <hirenkavad@gmail.com>
2020-08-31 23:40:06 +0530

    payout

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,17 +33,17 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             $CronsController=new CronsController;
             $CronsController->backupDatabase();
-        })->dailyAt('23:09');
+        })->dailyAt('23:55');
 
         $schedule->call(function () {
             $CronsController=new CronsController;
             $CronsController->generateMonthlyPayout();
-        })->monthlyOn(31, '23:00');
+        })->monthlyOn(31, '23:50');
 
         $schedule->call(function () {
             $CronsController=new CronsController;
             $CronsController->WallOfWyntashReport();
-        })->monthlyOn(31, '23:10');
+        })->monthlyOn(31, '23:55');
 
         // $schedule->call(function () {
         //     $CronsController=new CronsController;
