fd1c79e1f4c3dcf3486a7c3fefb0c050edf30051
root <hirenkavad@gmail.com>
2020-10-01 01:09:04 +0530

    setting crons again

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -36,15 +36,15 @@ class Kernel extends ConsoleKernel
             $CronsController->backupDatabase();
         })->dailyAt('23:55');
 
-        // $schedule->call(function () {
-        //     $CronsController=new CronsController;
-        //     $CronsController->generateMonthlyPayout();
-        // })->monthlyOn(1, '00:01');
+        $schedule->call(function () {
+            $CronsController=new CronsController;
+            $CronsController->generateMonthlyPayout();
+        })->monthlyOn(2, '00:01');
 
         $schedule->call(function () {
             $CronsController=new CronsController;
             $CronsController->WallOfWyntashReport();
-        });
+        })->monthlyOn(2, '00:20');
     }
 
     /**
